1c0ef758e16511069cbc54f11a8208112cd59e30
ossamaelkhal <ossamaelkhal@gmail.com>
2025-09-22 20:04:25 +0000

    up

--- SinergIA/src/components/NicheSolutionsModal.jsx
+++ SinergIA/src/components/NicheSolutionsModal.jsx
@@ -1,1 +1,15 @@
-Inherited file content will not be shown
+import React from 'react';
+
+const NicheSolutionsModal = ({ closeModal }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Soluções de Nicho</h2>
+        <p>Este é o conteúdo do modal de soluções de nicho.</p>
+        <button onClick={closeModal}>Fechar</button>
+      </div>
+    </div>
+  );
+};
+
+export default NicheSolutionsModal;
