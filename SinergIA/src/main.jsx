725855d1e27d328e33ce4b1fa30fa25b868e52c6
Ossama El Khal <ossamaelkhal@gmail.com>
2025-09-22 19:02:52 -0300

    up

--- SinergIA/src/main.jsx
+++ SinergIA/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
+import { APIProvider } from './contexts/APIContext';
 import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <APIProvider>
+      <App />
+    </APIProvider>
   </React.StrictMode>,
 );
